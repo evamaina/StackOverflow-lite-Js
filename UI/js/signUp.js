16dfc2d2e9e7955b3433a14143652d6252e8ac5e
Eva Maina <evajohnson714@gmail.com>
2018-09-12 17:48:30 +0300

    [Feature #160329243]Catch json response message

--- UI/js/signUp.js
+++ UI/js/signUp.js
@@ -2,7 +2,6 @@ document.getElementById('postData').addEventListener('submit', postData);
 
 function postData(event) {
             event.preventDefault();
-
             let first_name = document.getElementById("Fname").value;
             let last_name = document.getElementById("Lname").value;
             let username = document.getElementById("username").value;
@@ -19,17 +18,31 @@ function postData(event) {
                 "confirm_password":confirm_password   
                 })
 
-            
             fetch('http://127.0.0.1:5000/api/v2/signup', {
                 method: 'POST',
                 headers: {
-                    'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
                 body: user_data
-            }).then((response) => response.json())
-                .then((response) => alert('data sent'))
-                .catch((err) => console.log(err))
+            })
+                .then((response) => {
+                    statusCode = response.status
+                    return response.json()
+                })
+                .then((response) => {
+                    if (statusCode == 201){
+                        alert(response.Message)
+                        window.location = 'login.html';
+                    }
+                    if (statusCode == 400){
+                        alert(response.Message)
+                    }
+                    if (statusCode == 409){
+                        alert(response.Message)
+                    }
+                    console.log(response.Message)
+                })
+                .catch((err) => console.log('Eve says '+err))
                  
         }
                 
